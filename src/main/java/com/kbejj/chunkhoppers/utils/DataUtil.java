79160aaddee04306d18ebdd3c83c6f552ab30f6c
McDoJr <51730379+McDoJr@users.noreply.github.com>
2022-10-15 11:58:34 +0800

    DataUtil updated

--- src/main/java/com/kbejj/chunkhoppers/utils/DataUtil.java
+++ src/main/java/com/kbejj/chunkhoppers/utils/DataUtil.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,13 +14,18 @@ public class DataUtil {
         if(player.hasPermission("chunkhoppers.limit.*") || player.hasPermission("*") || player.isOp()){
             return Integer.MAX_VALUE;
         }
-        List<PermissionAttachmentInfo> attachment = player.getEffectivePermissions()
-                .stream().filter(attachmentInfo -> attachmentInfo.getPermission().startsWith("chunkhoppers.limit.")).sorted().collect(Collectors.toList());
-        if(attachment.isEmpty()){
-            return 1;
+        return getPermissionValues(player);
+    }
+
+    private static int getPermissionValues(Player player){
+        List<Integer> values = new ArrayList<>();
+        for(PermissionAttachmentInfo attachment : player.getEffectivePermissions().stream()
+                .filter(permission -> permission.getPermission().startsWith("chunkhoppers.limit.")).collect(Collectors.toList())){
+            String permission = attachment.getPermission();
+            values.add(Integer.parseInt(permission.substring(permission.lastIndexOf(".") + 1)));
         }
-        String permission = attachment.get(attachment.size()-1).getPermission();
-        return Integer.parseInt(permission.substring(permission.lastIndexOf(".") + 1));
+        values = values.stream().sorted().collect(Collectors.toList());
+        return values.isEmpty() ? 1 : values.get(values.size() - 1);
     }
 
     public static boolean hasShopGuiPlus(){
